ed72b47182adaf97e9ca011575079361156dc8ff
Katsumi-N <61781055+Katsumi-N@users.noreply.github.com>
2022-07-23 14:32:30 +0900

    discordクライアント追加

--- discord/main.go
+++ discord/main.go
@@ -4,14 +4,37 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"grpc-conoha/config"
 	conohapb "grpc-conoha/pkg/grpc"
 
+	"github.com/bwmarrin/discordgo"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
+	// discord bot
+	discordToken := config.Config.DiscordToken
+	dg, err := discordgo.New("Bot " + discordToken)
+	defer dg.Close()
+	// dg.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuilds | discordgo.IntentsGuildMessages)
+	dg.AddHandler(Minecraft)
+	err = dg.Open()
+	if err != nil {
+		log.Printf("error opening connection,", err)
+		return
+	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+}
+
+func Minecraft(s *discordgo.Session, m *discordgo.MessageCreate) {
 	address := "localhost:8080"
 	// gRPCサーバーとのコネクションを確立する
 	conn, err := grpc.Dial(
@@ -28,12 +51,14 @@ func main() {
 	client := conohapb.NewConohaServiceClient(conn)
 
 	req := &conohapb.MinecraftRequest{
-		Command: "!server",
+		Command: m.Content,
 	}
 	res, err := client.Minecraft(context.Background(), req)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
+	testMessage := "@here gRPCこのはちゃんどりゃあああああああああああああ"
+	s.ChannelMessageSend(m.ChannelID, testMessage)
 	fmt.Println(res.GetIsNormal(), res.GetMessage())
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	cloud.google.com/go v0.56.0 // indirect
+	github.com/bwmarrin/discordgo v0.25.0 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.1 // indirect
 	github.com/cncf/udpa/go v0.0.0-20210930031921-04548b0d99d4 // indirect
@@ -12,7 +13,9 @@ require (
 	github.com/envoyproxy/protoc-gen-validate v0.1.0 // indirect
 	github.com/fullstorydev/grpcurl v1.8.6 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/jhump/protoreflect v1.10.3 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
 	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
